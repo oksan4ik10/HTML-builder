21498492f84864c5dbf902764090cf24c70f3ec3
Oksana Kalinina <ksan4ik92@gmail.com>
2022-11-03 10:06:15 +0300

    fix:04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,18 +5,7 @@ fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
   if (err) throw err;
   fs.readdir(path.join(__dirname, 'files'), (err, files) => {
     files.forEach((file) => {
-      const rr = fs.ReadStream(path.join(__dirname, 'files', file), 'utf-8');
-
-
-      rr.on('readable', () => {
-        const data = rr.read();
-        if (data != null) {
-          const rw = fs.createWriteStream(path.join(__dirname, 'files-copy', file), 'utf-8');
-          rw.write(data)
-
-        }
-      });
-
+      fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => { if (err) throw err })
     })
   })
 });
