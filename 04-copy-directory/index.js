73f9ee44054fcc6e8e7552d6371e78da919481fe
Oksana Kalinina <ksan4ik92@gmail.com>
2022-11-08 23:46:41 +0300

    fix:delete file

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,6 +3,12 @@ const path = require('path');
 
 fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
   if (err) throw err;
+  fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+    files.forEach((file) => {
+      fs.unlink(path.join(__dirname, 'files-copy', file), (err) => { if (err) throw err })
+    })
+  })
+
   fs.readdir(path.join(__dirname, 'files'), (err, files) => {
     files.forEach((file) => {
       fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (err) => { if (err) throw err })
